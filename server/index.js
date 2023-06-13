1b29355573e8bfc66fecceb0a91f4aca8a7f3246
Abhishek pandey <abhie9336@gmail.com>
2023-06-13 16:15:26 +0530

    setting routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connedtDB from "./mongodb/connect.js";
+
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -11,6 +15,9 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({limit: "50mb"}));
+
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
     res.send("Hello world");
