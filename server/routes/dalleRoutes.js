54e7d34b88c2db35e6d3950d67a913a69ea3b62a
Abhishek pandey <abhie9336@gmail.com>
2023-06-13 16:30:04 +0530

    photo generating api complete

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,4 +16,25 @@ router.route('/').get((req,res) =>{
     res.send("hello from dall e");
 })
 
+router.route('/').post(async (req,res) =>{
+    try{
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        })
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({photo:image});
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).send(err?.response.data.error.message)
+    }
+})
+
 export default router;
