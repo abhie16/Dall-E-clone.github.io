f6e678363c1152a9731ccf7085427cc561aec423
Abhishek pandey <abhie9336@gmail.com>
2023-06-14 06:24:55 +0530

    connected cloudinary to store posts

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,24 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+// get all posts
+
+router.route('/').get(async (req, res) => {
+
+});
+
+// create a post
+
+router.route('/').post(async (req, res) => {
+    const {name, prompt, photo} = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+});
+
+
 export default router;
